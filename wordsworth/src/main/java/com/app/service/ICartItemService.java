a543e8c51cb92a4d10e06bfcfbf1b73f013ec236
final-project-pgdac-02 <project.pgdac@gmail.com>
2022-04-02 21:12:54 +0530

    added new comment from shivam

--- wordsworth/src/main/java/com/app/service/ICartItemService.java
+++ wordsworth/src/main/java/com/app/service/ICartItemService.java
@@ -3,5 +3,5 @@ package com.app.service;
 public interface ICartItemService {
 	void saveToCart(Integer userId,Integer bookId,int quantity); //create cartItem obj -> fetch book by Id, set book & set quantity in CartItem obj -> fetch user by id -> call user's helper method to add cart item
 	void incrementCartItem(Integer userId,Integer bookId);
-	void decrementCartItem(Integer userId,Integer bookId);
+	void decrementCartItem(Integer userId,Integer bookId); // need to be implemented!!!
 }
